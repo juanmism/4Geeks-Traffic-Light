834f21c9566d53cb036dadde1c1dad6a4e6030f4
juanmism <juanmism@gmail.com>
2022-11-29 20:25:57 +0000

    fin

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import "../../styles/home.css";
 
-//create your first component
-const Home = () => {
+function Home() {
+	const [lightPus, listChange] = useState([
+		"redLigthFlash",
+		"yellowLigth",
+		"greenLigth",
+	]);
+
+	function redLightPower() {
+		listChange(["redLigthFlash", "yellowLigth", "greenLigth"]);
+	}
+	function yellowLightPower() {
+		listChange(["redLigth", "yellowLigthFlash", "greenLigth"]);
+	}
+	function greenLightPower() {
+		listChange(["redLigth", "yellowLigth", "greenLigthFlash"]);
+	}
+	console.log(lightPus);
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div id="padre">
+			<div id="container">
+				<div className={lightPus[0]} onClick={redLightPower}></div>
+				<div className={lightPus[1]} onClick={yellowLightPower}></div>
+				<div className={lightPus[2]} onClick={greenLightPower}></div>
+			</div>
 		</div>
 	);
-};
+}
 
 export default Home;
